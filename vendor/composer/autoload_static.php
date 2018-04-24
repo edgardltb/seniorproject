88ce0b429a24a40987eff8d90bc8723178598e55
Edgar <edgardlt@gmail.com>
2018-04-23 21:40:13 -0500

    commited

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -82,12 +82,66 @@ class ComposerStaticInitd4fd0bfe117048aced95f2074585df9a
         ),
     );
 
+    public static $classMap = array (
+        'Administrator' => __DIR__ . '/../..' . '/models/Administrator.php',
+        'AdministratorQuery' => __DIR__ . '/../..' . '/models/AdministratorQuery.php',
+        'Base\\Administrator' => __DIR__ . '/../..' . '/models/Base/Administrator.php',
+        'Base\\AdministratorQuery' => __DIR__ . '/../..' . '/models/Base/AdministratorQuery.php',
+        'Base\\Category' => __DIR__ . '/../..' . '/models/Base/Category.php',
+        'Base\\CategoryQuery' => __DIR__ . '/../..' . '/models/Base/CategoryQuery.php',
+        'Base\\Customer' => __DIR__ . '/../..' . '/models/Base/Customer.php',
+        'Base\\CustomerHasQuestions' => __DIR__ . '/../..' . '/models/Base/CustomerHasQuestions.php',
+        'Base\\CustomerHasQuestionsQuery' => __DIR__ . '/../..' . '/models/Base/CustomerHasQuestionsQuery.php',
+        'Base\\CustomerQuery' => __DIR__ . '/../..' . '/models/Base/CustomerQuery.php',
+        'Base\\Media' => __DIR__ . '/../..' . '/models/Base/Media.php',
+        'Base\\MediaQuery' => __DIR__ . '/../..' . '/models/Base/MediaQuery.php',
+        'Base\\Mentor' => __DIR__ . '/../..' . '/models/Base/Mentor.php',
+        'Base\\MentorQuery' => __DIR__ . '/../..' . '/models/Base/MentorQuery.php',
+        'Base\\Questions' => __DIR__ . '/../..' . '/models/Base/Questions.php',
+        'Base\\QuestionsQuery' => __DIR__ . '/../..' . '/models/Base/QuestionsQuery.php',
+        'Base\\Schedule' => __DIR__ . '/../..' . '/models/Base/Schedule.php',
+        'Base\\ScheduleQuery' => __DIR__ . '/../..' . '/models/Base/ScheduleQuery.php',
+        'Base\\User' => __DIR__ . '/../..' . '/models/Base/User.php',
+        'Base\\UserInfo' => __DIR__ . '/../..' . '/models/Base/UserInfo.php',
+        'Base\\UserInfoQuery' => __DIR__ . '/../..' . '/models/Base/UserInfoQuery.php',
+        'Base\\UserQuery' => __DIR__ . '/../..' . '/models/Base/UserQuery.php',
+        'Category' => __DIR__ . '/../..' . '/models/Category.php',
+        'CategoryQuery' => __DIR__ . '/../..' . '/models/CategoryQuery.php',
+        'Customer' => __DIR__ . '/../..' . '/models/Customer.php',
+        'CustomerHasQuestions' => __DIR__ . '/../..' . '/models/CustomerHasQuestions.php',
+        'CustomerHasQuestionsQuery' => __DIR__ . '/../..' . '/models/CustomerHasQuestionsQuery.php',
+        'CustomerQuery' => __DIR__ . '/../..' . '/models/CustomerQuery.php',
+        'Map\\AdministratorTableMap' => __DIR__ . '/../..' . '/models/Map/AdministratorTableMap.php',
+        'Map\\CategoryTableMap' => __DIR__ . '/../..' . '/models/Map/CategoryTableMap.php',
+        'Map\\CustomerHasQuestionsTableMap' => __DIR__ . '/../..' . '/models/Map/CustomerHasQuestionsTableMap.php',
+        'Map\\CustomerTableMap' => __DIR__ . '/../..' . '/models/Map/CustomerTableMap.php',
+        'Map\\MediaTableMap' => __DIR__ . '/../..' . '/models/Map/MediaTableMap.php',
+        'Map\\MentorTableMap' => __DIR__ . '/../..' . '/models/Map/MentorTableMap.php',
+        'Map\\QuestionsTableMap' => __DIR__ . '/../..' . '/models/Map/QuestionsTableMap.php',
+        'Map\\ScheduleTableMap' => __DIR__ . '/../..' . '/models/Map/ScheduleTableMap.php',
+        'Map\\UserInfoTableMap' => __DIR__ . '/../..' . '/models/Map/UserInfoTableMap.php',
+        'Map\\UserTableMap' => __DIR__ . '/../..' . '/models/Map/UserTableMap.php',
+        'Media' => __DIR__ . '/../..' . '/models/Media.php',
+        'MediaQuery' => __DIR__ . '/../..' . '/models/MediaQuery.php',
+        'Mentor' => __DIR__ . '/../..' . '/models/Mentor.php',
+        'MentorQuery' => __DIR__ . '/../..' . '/models/MentorQuery.php',
+        'Questions' => __DIR__ . '/../..' . '/models/Questions.php',
+        'QuestionsQuery' => __DIR__ . '/../..' . '/models/QuestionsQuery.php',
+        'Schedule' => __DIR__ . '/../..' . '/models/Schedule.php',
+        'ScheduleQuery' => __DIR__ . '/../..' . '/models/ScheduleQuery.php',
+        'User' => __DIR__ . '/../..' . '/models/User.php',
+        'UserInfo' => __DIR__ . '/../..' . '/models/UserInfo.php',
+        'UserInfoQuery' => __DIR__ . '/../..' . '/models/UserInfoQuery.php',
+        'UserQuery' => __DIR__ . '/../..' . '/models/UserQuery.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitd4fd0bfe117048aced95f2074585df9a::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitd4fd0bfe117048aced95f2074585df9a::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitd4fd0bfe117048aced95f2074585df9a::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitd4fd0bfe117048aced95f2074585df9a::$classMap;
 
         }, null, ClassLoader::class);
     }
